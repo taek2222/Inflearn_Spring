3e1447d7cf63ab663495eb0ddaf9d7dc6944ab7f
taek2222 <oot309@daum.net>
2024-06-20 02:46:38 +0900

    test: List, Map 을 활용해 다수 테스트 추가(#20)

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -21,4 +21,7 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
