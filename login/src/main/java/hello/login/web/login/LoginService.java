3ecf8c754463b0a583b5feb81048467ee364f01a
taek2222 <oot309@daum.net>
2024-07-23 16:07:51 +0900

    feat: 로그인 기능 추가 및 쿠키 사용 (#46)

---
+++ login/src/main/java/hello/login/web/login/LoginService.java
@@ -0,0 +1,25 @@
+package hello.login.web.login;
+
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *
+     * @param loginId 사용자 아이디
+     * @param password 사용자 패스워드
+     * @return
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
